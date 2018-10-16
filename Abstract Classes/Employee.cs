5bac87bc2c6e3c54c11f07224c6fc56766c69793
Ryan <werner.ryan18@gmail.com>
2018-10-16 12:42:24 -0700

    Add project files.

---
+++ Abstract Classes/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Classes
+{
+    public class Employee : Person
+    {
+        int ID { get; set; }
+
+        public override void SayName()
+        {
+            base.SayName();
+        }
+
+    }
+}
